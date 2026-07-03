7031d1e4cb01c1c98d9188c8ee77c6419c4d05f3
Elestraza <gameswithiy@gmail.com>
2026-07-03 17:59:39 +0300

    FederalRegion Repo

---
+++ src/Goods.Services/Products/Repositories/IFederalRegionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Goods.Domain.Products;
+
+namespace Goods.Services.Products.Repositories;
+
+public interface ICarCodesRepository
+{
+    Task SaveCarCode(CarCodes blank);
+    Task<CarCodes?> GetCarCode(Guid id);
+    Task<CarCodes?> GetCarCode(String name);
+    Task<Page<CarCodes>> GetCarCodes(Int32 page, Int32 count);
+    Task RemoveCarCode(Guid id);
+}
