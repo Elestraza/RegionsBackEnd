e710d468823a16511c6a2b9a614d91a4f890a35c
Elestraza <gameswithiy@gmail.com>
2026-07-07 17:53:51 +0300

    CHECK settlementstype field

--- src/Goods.Migrator/Migrations/20260707/202606070913 CREATE TABLE settlementstypes.sql
+++ src/Goods.Migrator/Migrations/20260707/202606070913 CREATE TABLE settlementstypes.sql
@@ -1,4 +1,4 @@
 CREATE TABLE settlementstypes (
 	id uuid primary key NOT NULL,
-  	settlementstype varchar NOT NULL
+  	settlementstype varchar NOT NULL CHECK(settlementstype = 'город' OR settlementstype = 'деревня' OR settlementstype = 'село')
 );
