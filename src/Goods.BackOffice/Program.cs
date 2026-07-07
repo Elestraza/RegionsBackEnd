3a87fc002527a5a34a320f75a59d378db8519ee6
Elestraza <gameswithiy@gmail.com>
2026-07-07 14:04:28 +0300

    StartUp changes

--- src/Goods.BackOffice/Program.cs
+++ src/Goods.BackOffice/Program.cs
@@ -2,6 +2,7 @@ using Goods.Services;
 using Goods.Tools.Utils.Json;
 using Microsoft.AspNetCore.ResponseCompression;
 using System.IO.Compression;
+using static Microsoft.AspNetCore.Http.StatusCodes;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +28,15 @@ builder.Host.ConfigureServices((_, serviceCollection) =>
         });
     }
 );
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 
 WebApplication app = builder.Build();
 
@@ -34,6 +44,8 @@ app.UseResponseCompression();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
