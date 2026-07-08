c3506ecb9720fecb3d84d06268748a4f78eb6746
Elestraza <gameswithiy@gmail.com>
2026-07-08 11:28:13 +0300

    Changes in router

--- src/Goods.BackOffice/client-app/src/app/router.tsx
+++ src/Goods.BackOffice/client-app/src/app/router.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { SettlementsPage } from './settlements/settlementsPage';
-import { SettlementsTypesPage } from './settlementsTypes/settlementsTypesPage';
 import { RegionsPage } from './regions/regionsPage';
-import { FederalRegionsPage } from './federalRegions/federalRegionsPage';
 import { CarCodesPage } from './carCodes/carCodesPage'
 
 
@@ -11,9 +9,7 @@ export function Router() {
 	return (
 		<>
 			<Route path='settlements' element={<SettlementsPage />} />
-			<Route path='settlements-types' element={<SettlementsTypesPage />} />
 			<Route path='regions' element={<RegionsPage />} />
-			<Route path='federal-regions' element={<FederalRegionsPage />} />
 			<Route path='car-codes' element={<CarCodesPage />} />
 		</>
 	);
@@ -21,8 +17,6 @@ export function Router() {
 
 export class Links {
 	public static settlements = "settlements";
-	public static settlementsTypes = "settlements-types";
 	public static regions = "regions";
-	public static federalRegions = "federal-regions";
 	public static carCodes = "car-codes";
 }
